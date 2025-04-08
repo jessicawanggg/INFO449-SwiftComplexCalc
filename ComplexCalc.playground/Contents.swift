8ba619e7122e2075783422b2600340cd870b3ace
Jessica Wang <jessicawang@Jessicas-MacBook-Pro.local>
2025-04-07 22:43:00 -0700

    First half completed

--- ComplexCalc.playground/Contents.swift
+++ ComplexCalc.playground/Contents.swift
@@ -28,6 +28,50 @@ print("Welcome back to the UW Calculator")
 //: IMPORTANT: If any tests are commented out, you will be graded a zero (0)! You should never be in the habit of eliminating tests to make the code pass.
 //:
 class Calculator {
+//    var inputArray : [Int]
+//    init(input : [Int]) {
+//        self.inputArray = input
+//    }
+    
+    // Simple operations with integer inputs
+    func add(lhs : Int, rhs : Int) -> Int {
+        return lhs + rhs
+    }
+    func subtract(lhs : Int, rhs : Int) -> Int {
+        return lhs - rhs
+    }
+    func multiply(lhs : Int, rhs : Int) -> Int {
+        return lhs * rhs
+    }
+    func divide(lhs : Int, rhs : Int) -> Int {
+        return lhs / rhs
+    }
+    
+    func mathOp(lhs : Int, rhs : Int, op : (Int, Int) -> Int) -> Int {
+        return op(lhs, rhs)
+    }
+    
+    // Complex operations with array inputs
+    func add(_ inputArray : [Int]) -> Int {
+        var sum : Int = 0
+        for num in inputArray {
+            sum += num
+        }
+        return sum
+    }
+    func multiply(_ inputArray : [Int]) -> Int {
+        var product : Int = 1
+        for num in inputArray {
+            product *= num
+        }
+        return product
+    }
+    func count(_ inputArray : [Int]) -> Int {
+        return inputArray.count
+    }
+    func avg(_ inputArray : [Int]) -> Int {
+        return self.add(inputArray) / self.count(inputArray)
+    }
 }
 
 //: Don't change the name of this object (`calc`); it's used in all the tests.
@@ -43,6 +87,9 @@ let calc = Calculator()
 //: Keep in mind that writing new tests may reveal ambiguity in the specification above--if that's the case, document the ambiguity, declare what you think *should* happen, and write the test to test for it.
 
 // ===== Your tests go here
+// CHECK EDGE CASES FOR DIVISION
+// non-numeric input
+// guard against lack of input / empty input
 
 //: ---
 //: ## Test code block
@@ -52,7 +99,7 @@ calc.subtract(lhs: 2, rhs: 2) == 0
 calc.multiply(lhs: 2, rhs: 2) == 4
 calc.divide(lhs: 2, rhs: 2) == 1
 
-calc.mathOp(lhs: 5, rhs: 5, op: { (lhs: Int, rhs: Int) -> Int in (lhs + rjs) + (lhs * rhs) }) == 35
+calc.mathOp(lhs: 5, rhs: 5, op: { (lhs: Int, rhs: Int) -> Int in (lhs + rhs) + (lhs * rhs) }) == 35
     // This style is one way of writing an anonymous function
 calc.mathOp(lhs: 10, rhs: -5, op: { ($0 + $1) + ($0 - $1) }) == 20
     // This is the second, more terse, style; either works
@@ -64,24 +111,24 @@ calc.count([]) == 0
 calc.avg([2, 2, 2, 2, 2, 2]) == 2
 calc.avg([1, 2, 3, 4, 5]) == 3
 calc.avg([1]) == 1
-
-calc.mathOp(args: [1, 2, 3], beg: 0, op: { $0 + $1 }) == 6
-    // this is (((0 op 1) op 2) op 3)
-calc.mathOp(args: [1, 2, 3, 4, 5], beg: 0, op: { $0 + $1 }) == 15
-    // this is (((((0 op 1) op 2) op 3) op 4) op 5)
-calc.mathOp(args: [1, 1, 1, 1, 1], beg: 1, op: { $0 * $1 }) == 1
-    // this is (((((1 op 1) op 1) op 1) op 1) op 1)
-
-let p1 = (5, 5)
-let p2 = (12, -27)
-let p3 = (-4, 4)
-let p4 = (0, 0)
-calc.add(lhs: p1, rhs: p2) == (17, -22)
-calc.subtract(lhs: p1, rhs: p2) == (-7, 32)
-calc.add(lhs: p4, rhs: p4) == (0, 0)
-calc.add(lhs: p3, rhs: p4) == (-4, 4)
-
-let pd1 = ["x": 5, "y": 5]
-let pd2 = ["x": -4, "y": 4]
-calc.add(lhs: pd1, rhs: pd2) == ["x": 1, "y": 9]
-calc.subtract(lhs: pd1, rhs: pd2) == ["x": 9, "y": 1]
+//
+//calc.mathOp(args: [1, 2, 3], beg: 0, op: { $0 + $1 }) == 6
+//    // this is (((0 op 1) op 2) op 3)
+//calc.mathOp(args: [1, 2, 3, 4, 5], beg: 0, op: { $0 + $1 }) == 15
+//    // this is (((((0 op 1) op 2) op 3) op 4) op 5)
+//calc.mathOp(args: [1, 1, 1, 1, 1], beg: 1, op: { $0 * $1 }) == 1
+//    // this is (((((1 op 1) op 1) op 1) op 1) op 1)
+//
+//let p1 = (5, 5)
+//let p2 = (12, -27)
+//let p3 = (-4, 4)
+//let p4 = (0, 0)
+//calc.add(lhs: p1, rhs: p2) == (17, -22)
+//calc.subtract(lhs: p1, rhs: p2) == (-7, 32)
+//calc.add(lhs: p4, rhs: p4) == (0, 0)
+//calc.add(lhs: p3, rhs: p4) == (-4, 4)
+//
+//let pd1 = ["x": 5, "y": 5]
+//let pd2 = ["x": -4, "y": 4]
+//calc.add(lhs: pd1, rhs: pd2) == ["x": 1, "y": 9]
+//calc.subtract(lhs: pd1, rhs: pd2) == ["x": 9, "y": 1]
